9f0811c0cd510d8a963176fbf216fefde869bd8f
David Otiz <david.ortiz.p@gmail.com>
2021-12-13 17:03:12 -0500

    first commit

---
+++ log.md
@@ -0,0 +1,9 @@
+[x] - Get MySQL instance with docker-compose
+
+[x] - MySQL client Table Plus install
+
+[x] - Node's MySQL client mysql2 install
+
+[x] - Database ORM for Node Sequelize
+
+[x] - Create database
---
+++ migrations/20211124011451-create-products-table.js
@@ -0,0 +1,28 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Products", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      image: Sequelize.STRING,
+      name: Sequelize.STRING,
+      brand: Sequelize.STRING,
+      presentation: Sequelize.STRING,
+      price: Sequelize.DECIMAL(11, 4),
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Products");
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};
